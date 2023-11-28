8e58fb08d25a4e036d30b10104f41bdc900412da
Luca Ambrosi <luca@luca.it>
2023-11-28 21:56:51 +0100

    new route sendemail

--- netlify/functions/sendmail.js
+++ netlify/functions/sendmail.js
@@ -1,7 +1,9 @@
 const emailjs = require("@emailjs/nodejs")
 
 exports.handler = async function (event, context) {
-    const response = await emailjs.send('service_vsq3w3a', 'template_42ssg8p', {}, {publicKey: '970IaolZgQOrCiJGD', privateKey: 'qu7z0rQZUX6mAW_YvI8xl'})
+    const response = await emailjs.send('service_vsq3w3a', 'template_42ssg8p', {}, {
+        publicKey: '970IaolZgQOrCiJGD', privateKey: 'qu7z0rQZUX6mAW_YvI8xl'
+    })
 
     return {
         statusCode: 200,
